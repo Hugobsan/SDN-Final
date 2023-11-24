05d5afa3b317416858faf4b1de7041fb967b11b5
Hugobsan <hugocorone10@gmail.com>
2023-11-24 12:16:51 -0300

    desistindo da função de plotar o desenho

--- principal.py
+++ principal.py
@@ -5,9 +5,6 @@ from mininet.cli import CLI
 from mininet.link import TCLink
 from mininet.log import setLogLevel
 
-#printar a topologia da rede no terminal
-#sudo mn --custom principal.py --topo mytopo --test pingall
-
 # Criar a rede
 def create_network():
     net = Mininet(controller=Controller, switch=OVSSwitch)
@@ -67,28 +64,7 @@ def create_network():
     s3.cmd('ovs-vsctl set-controller s3 tcp:127.0.0.1:6633')
     s4.cmd('ovs-vsctl set-controller s3 tcp:127.0.0.1:6633')
     s5.cmd('ovs-vsctl set-controller s3 tcp:127.0.0.1:6633')
-        
-    import networkx as nx
-
-    def print_network_topology(net):
-        G = nx.Graph()
-        
-        # Adicione os nós à topologia
-        for host in net.hosts:
-            G.add_node(host.name)
-        
-        # Adicione as arestas à topologia
-        for link in net.links:
-            G.add_edge(link.intf1.node.name, link.intf2.node.name)
-        
-        # Imprima a topologia de rede
-        print("Network Topology:")
-        print("Nodes:", G.nodes)
-        print("Edges:", G.edges)
-
-    # Chame a função para imprimir a topologia de rede
-    print_network_topology(net)
-
+    
     # Iniciar a interface de linha de comando
     CLI(net)
 
