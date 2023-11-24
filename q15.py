a70f8e60fd4befca1fc52a90a4d61dc7a7d0334b
Hugobsan <hugocorone10@gmail.com>
2023-11-24 14:59:57 -0300

    up

--- q15.py
+++ q15.py
@@ -33,11 +33,9 @@ def create_network():
 
     # Conectar os switches
     net.addLink(s1, s4)
-    net.addLink(s4, s3)
-    net.addLink(s3, s2)
-    net.addLink(s2, s5)
-    net.addLink(s5, s3)
-    net.addLink(s3, s1)
+    net.addLink(s1, s5)
+    net.addLink(s5, s2)
+    net.addLink(s2, s3)
 
     # Iniciar a rede
     net.build()
